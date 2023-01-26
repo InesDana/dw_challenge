f5a9bc86a91df81f359fd66098124c1e1a919633
Dana Walker <danaehlert@yahoo.de>
2023-01-26 09:48:33 +0100

    change table name

--- t1_csv_to_sql.py
+++ t1_csv_to_sql.py
@@ -10,16 +10,16 @@ engine = create_engine('postgresql://postgres:assessment2023@localhost:5432/DWch
 # iterate over files in directory
 for filename in os.scandir(directory):
     if filename.is_file() and filename.name.endswith(".csv") :
-        #print(filename.path, filename.name)
         df=pd.read_csv(filename)
         # Save the data from dataframe to postgres
+        table_name='report_'+filename.name[:4]+'_'+filename.name[5:7]+'_'+filename.name[8:10]
         df.to_sql(
-            str(filename.name[:10]),
+            table_name,
             engine,
             index=False,  # Not copying over the index
-            if_exists='replace'
+            if_exists='replace' # replace table
         )
-        print(filename.name[:10])
+        print(table_name+' table saved to database')
 
 
 
